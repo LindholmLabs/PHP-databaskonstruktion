f2f3a4484f28dfb69db1a4649c35a708f296bef3
William Lindholm <william_lindholm@outlook.com>
2023-09-27 18:24:22 +0000

    Added inserthandler.

--- incidents.php
+++ incidents.php
@@ -1,19 +1,23 @@
 <?php
     require 'imports.php';
     dbconnection::getInstance('mysql', 'a22willi', 'root', 'Safiren1');
+    $handlerFactory = new InsertHandlerFactory();
 
     $pageContent = '';
 
-    $pageContent .= "<h3>FieldAgents</h3>" . tableFactory::createTable("Incident");
+    $pageContent .= "<h3>Incident</h3>" . tableFactory::createTable("Incident");
 
     $modalBuilder = (new ModalBuilder())
             ->setModalId('insertModal')
             ->setTableName("Incident")
+            ->setInsertHandler($handlerFactory->createHandler('Incident'))
             ->addColumn("RegionName")
             ->addColumn("Location")
-            ->addDropdownColumn("Incident", GetCompositeKeyValues("Incident", ["IncidentName", "IncidentNumber"])) #add GetCompositeKeyValues(table, [column1, column2...]); separate using ", "
+            ->addColumn("IncidentName")
+            ->addColumn("IncidentNumber")
             ->addDropdownColumn("Terrain", getColumnValues("Terrain", "TerrainCode"));
 
+    $modalBuilder->handleData();
     $pageContent .= $modalBuilder->build();
     $pageContent .= $modalBuilder->generateOpenButton("Create incident");
 

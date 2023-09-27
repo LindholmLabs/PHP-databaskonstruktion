118a1b933ba4c7a821b602810c97fc86f4fcaeb1
William Lindholm <william_lindholm@outlook.com>
2023-09-27 18:19:33 +0000

    Logging to file instead of using JS console log.

--- logging.php
+++ logging.php
@@ -1,15 +1,7 @@
 <?php
     function logg($message) {
-        global $console_logs;
-        $console_logs[] = $message;
-    }
-    
-    function outputConsoleLogs() {
-        global $console_logs;
-        echo '<script>';
-        foreach ($console_logs as $msg) {
-            echo "console.log(" . json_encode($msg) . ");";
-        }
-        echo '</script>';
+        $timestamp = date("Y-m-d H:i:s");
+        $logMessage = "{$timestamp} - {$message}\n";
+        file_put_contents('logs.txt', $logMessage, FILE_APPEND);
     }
 ?>
